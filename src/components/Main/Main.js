4389bbf3f001a8375c0ca3d3382af7247d339f11
Mechwarrior21 <kevinkkan@rogers.com>
2017-04-01 17:26:03 +0800

    made battle struture view on main page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,16 +4,25 @@ import PokeList from './PokeList/PokeList';
 import Navbar from './Navbar/Navbar';
 import Search from './Search/Search';
 
+import Portrait from '../Battle/Portrait/Portrait';
+import Health from '../Battle/Health/Health';
+import Stats from '../Battle/Stats/Stats';
+
 class Main extends Component {
 
   render(){
     return (
       <div>
-        <Navbar />
-        <div className='container'>
-          <Search />
-          <PokeList />
-        </div>
+          <div>
+            <Navbar />
+            <div className='container'>
+              <Search />
+              <PokeList />
+            </div>
+          </div>
+          <Portrait/>
+          <Health/>
+          <Stats/>
       </div>
     )
   }
