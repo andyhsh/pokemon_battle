7ae3c759b66fcc3c6a210f995c0ec1f2d1903a5e
Mechwarrior21 <kevinkkan@rogers.com>
2017-04-01 13:54:23 +0800

    add basic code structure

---
+++ src/components/Battle/BattlePage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+export default Battlepage;
---
+++ src/components/Main/Choice/Choice.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Create  extends Component {
+
+  render() {
+
+    return(
+
+    )
+  }
+}
+
+export default connect()(Choice);
