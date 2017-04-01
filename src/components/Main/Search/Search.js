d5c2c35792f025492214f60496c418e13641b0e4
Andy Ho <89andy@gmail.com>
2017-04-01 16:18:21 +0800

    searchbar logging values

--- src/components/Main/Search/Search.js
+++ src/components/Main/Search/Search.js
@@ -3,15 +3,20 @@ import { connect } from 'react-redux';
 
 class Create  extends Component {
 
-  render() {
+  //change state for redux on user inputs
+  handleChange = (e) => {
+    console.log(e.target.value);
+  }
 
+  render() {
     return(
     	<div className="row">
-        <div className="col-md-4">
+        <div className="col-md-4 col-md-offset-4">
           <div className="input-group col-md-12">
-              <input type="text" className="form-control input-lg" placeholder="Search Pokemon" />
+              <input type="text" className="form-control input" placeholder="Search Pokemon"
+               onChange={this.handleChange} />
               <span className="input-group-btn">
-                  <button className="btn btn-info btn-lg" type="button">
+                  <button className="btn btn-default" type="button">
                       <i className="glyphicon glyphicon-search"></i>
                   </button>
               </span>
