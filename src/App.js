d8d6dadcffa6f4a76921263805c4aa6c5e136c1e
Mechwarrior21 <kevinkkan@rogers.com>
2017-04-04 15:47:48 +0800

    login for google in navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import Main from './components/Main/Main';
-import * as firebase from 'firebase';
 //to import react-router-dom here
-
-//Initialise Firebase
-var config = {
-  apiKey: "AIzaSyCCwV3k-R-ByI3uEcoQhxVumO8J-F2Rs3E",
-  authDomain: "pokemonbattle-2dbd2.firebaseapp.com",
-  databaseURL: "https://pokemonbattle-2dbd2.firebaseio.com",
-  storageBucket: "pokemonbattle-2dbd2.appspot.com",
-};
-firebase.initializeApp(config);
 
 // Get load / save functions from the API
 //import {  } from './api/api'
--- src/components/Main/Navbar/Navbar.js
+++ src/components/Main/Navbar/Navbar.js
@@ -1,33 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
+import * as firebase from 'firebase';
 
 import './navbar.css';
 
-// Google provider object
-var provider = new firebase.auth.GoogleAuthProvider();
+var config = {
+  apiKey: "AIzaSyCCwV3k-R-ByI3uEcoQhxVumO8J-F2Rs3E",
+  authDomain: "pokemonbattle-2dbd2.firebaseapp.com",
+  databaseURL: "https://pokemonbattle-2dbd2.firebaseio.com",
+  storageBucket: "pokemonbattle-2dbd2.appspot.com",
+};
+firebase.initializeApp(config);
 
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-  }
-  // The signed-in user info.
-  var user = result.user;
-  }).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-});
+class Navbar extends Component {
 
-const Navbar = () => {
+  handleClick = (e) => {
+    // Google provider object
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().getRedirectResult(provider).then(function(result) {
+      if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+      }
+      // The signed-in user info.
+      var user = result.user;
+      }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+    });
+  };
+
+  render() {
     return (
       <header className='navbar navbar-default navbar-static-top'>
         <nav className='container'>
           <div className='navbar-header'>
             <a className='navbar-brand' href='#'>PokeGYM</a>
+          </div>
+          <div className='col-md-2'>
+            <button type='submit'
+                    className='btn btn-default'
+                    onClick={this.handleClick}>Log In</button>
           </div>
           <div className='nav'>
             <ul className='nav navbar-nav navbar-right'>
@@ -35,10 +53,10 @@ const Navbar = () => {
               <li><a href='#'>About</a></li>
             </ul>
           </div>
-          <button type="submit" className="btn btn-primary btn-lg btn-block">Log In</button>
         </nav>
       </header>
     )
+  }
 }
 
 export default Navbar;
